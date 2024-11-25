5d2fd94e1710dc71d2f98d776083aec8f09f7c8d
Fulano de Tal <gabriel.andrade200p@gmail.com>
2024-11-25 18:54:53 -0300

    arrumando resposividade 2

--- src/app/sections/Hero.tsx
+++ src/app/sections/Hero.tsx
@@ -4,10 +4,10 @@ import Image from "next/image";
 export default function Hero() {
   return (
     <>
-      <header className="p-4 md:p-16 flex md:flex-row flex-col items-center gap-8 md:gap-12 2xl:gap-20">
+      <header className="p-3 md:p-16 flex md:flex-row flex-col items-center gap-8 md:gap-12 2xl:gap-20">
         <Image src={Capa} alt="logo" className="w-[80%] md:w-1/3" />
         <div className="flex flex-col justify-center items-center gap-8 md:gap-20 2xl:gap-24">
-          <h1 className="text-primary text-[21px] md:text-4xl xl:text-5xl font-bold font-helvetica text-center md:text-pretty !leading-tight">
+          <h1 className="text-primary text-lg md:text-4xl xl:text-5xl font-bold font-helvetica text-center md:text-pretty !leading-tight">
             &quot;Às vezes, não importa quão alta seja a música ao nosso redor,
             é o coração que <br />
             dá o ritmo certo para seguir em frente.&quot;
@@ -18,7 +18,7 @@ export default function Hero() {
               href="https://www.xn--aultimadana-v9a.com/produtos/livro-a-ultima-danca-pre-venda/"
               target="_blank"
               rel="noreferrer"
-              className="bg-primary p-6 md:px-12 md:py-10 text-white font-bold text-2xl md:text-3xl 2xl:text-5xl rounded-xl md:rounded-3xl w-full text-center hover:bg-fuchsia-600 transition-colors duration-300 ease-in-out"
+              className="bg-primary p-6 md:px-12 md:py-10 text-white font-bold text-xl md:text-3xl 2xl:text-5xl rounded-xl md:rounded-3xl w-full text-center hover:bg-fuchsia-600 transition-colors duration-300 ease-in-out"
             >
               COMPRE O LIVRO FÍSICO
             </a>
@@ -27,7 +27,7 @@ export default function Hero() {
               href="https://www.amazon.com.br/Compre-a-%C3%BAltima-d%C3%A2ncia-Eduardo-Alves/dp/8546000002"
               target="_blank"
               rel="noreferrer"
-              className="bg-tertiary p-6 md:px-12 md:py-10 text-white font-bold text-2xl md:text-3xl 2xl:text-5xl rounded-xl md:rounded-3xl w-full text-center hover:bg-purple-700 transition-colors duration-300 ease-in-out"
+              className="bg-tertiary p-6 md:px-12 md:py-10 text-white font-bold text-xl md:text-3xl 2xl:text-5xl rounded-xl md:rounded-3xl w-full text-center hover:bg-purple-700 transition-colors duration-300 ease-in-out"
             >
               COMPRE NA AMAZON
             </a>
